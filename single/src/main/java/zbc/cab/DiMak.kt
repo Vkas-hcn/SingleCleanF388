90737b6498ee593cb382fcd6a2e8a1a04da88112
vkas <vkas@qq.com>
2025-11-14 18:05:18 +0800

    feat:guangbo-xiugai

--- single/src/main/java/zbc/cab/DiMak.kt
+++ single/src/main/java/zbc/cab/DiMak.kt
@@ -4,8 +4,8 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class DiMak : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-
-    }
-}
+//class DiMak : BroadcastReceiver() {
+//    override fun onReceive(context: Context?, intent: Intent?) {
+//
+//    }
+//}
